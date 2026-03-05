47d66bc6428ff3aeb1d5738c3c7cd35f339eaedc
Abdullah <abdulahwajid691@outlook.com>
2026-03-05 15:10:40 +0500

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import HistoryView from "./views/HistoryView";
 import QuizError from "./components/QuizError";
@@ -8,14 +8,14 @@ import Navbar from "./common/Navbar";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/QuizApp">
+      <HashRouter basename="/QuizApp">
         <Navbar />
         <Routes>
           <Route path="/*" element={<Home />} />
           <Route path="/History" element={<HistoryView props />} />
           <Route path="/QuizError" element={<QuizError />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
