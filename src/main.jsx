d0f20b89bd23f791f5098981980879b2dda3ccc3
Abdullah <abdulahwajid691@outlook.com>
2026-03-06 12:17:09 +0500

    main updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,16 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+const params = new URLSearchParams(window.location.search);
+const redirectPath = params.get("p");
 
-createRoot(document.getElementById("root")).render(<App />);
+if (redirectPath) {
+  window.history.replaceState(null, null, redirectPath);
+}
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
